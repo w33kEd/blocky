c9062a08932874c5f7e7240efb179d2979ae8f8d
w33ked <otieno.phidel11@gmail.com>
2026-01-18 17:14:50 +0300

    add transaction validation

--- wallet_server/wallet_server.go
+++ wallet_server/wallet_server.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"path"
 	"strconv"
 	"text/template"
 
+	"github.com/w33ked/go-blocky/utils"
 	"github.com/w33ked/go-blocky/wallet"
 )
 
@@ -51,8 +55,38 @@ func (ws *WalletServer) Wallet(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func (ws *WalletServer) CreateTransaction(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodPost:
+		decoder := json.NewDecoder(req.Body)
+		var t wallet.TransactionRequest
+		err := decoder.Decode(&t)
+		if err != nil {
+			log.Printf("ERR! %v", err)
+			io.WriteString(w, string(utils.JsonStatus("fail")))
+			return
+		}
+
+		if !t.Validate() {
+			log.Printf("ERR! Missing Fields!")
+			io.WriteString(w, string(utils.JsonStatus("fail")))
+			return
+		}
+
+		fmt.Println(*t.SenderPublicKey)
+		fmt.Println(*t.SenderBlockchainAddress)
+		fmt.Println(*t.SenderPrivateKey)
+		fmt.Println(*t.RecipientBlockchainAddress)
+		fmt.Println(*t.Value)
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		log.Println("ERR! Invalid HTTP Method")
+	}
+}
+
 func (ws *WalletServer) Run() {
 	http.HandleFunc("/", ws.Index)
 	http.HandleFunc("/wallet", ws.Wallet)
+	http.HandleFunc("/transaction", ws.CreateTransaction)
 	log.Fatal(http.ListenAndServe("0.0.0.0:"+strconv.Itoa(int(ws.Port())), nil))
 }
